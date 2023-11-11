19ab672729c0aa2cacd1791d4367b21cf188e749
YactayoC <syactayocrisostomo@gmail.com>
2023-11-10 20:01:47 -0500

    template MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import theme from "../theme";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
